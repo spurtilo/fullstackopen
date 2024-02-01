f5afa358933961fe67dc418a83b0155eead97327
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-02-01 13:20:45 +0200

    Add semicolons

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -1,59 +1,60 @@
-
 const Header = ({ courseInfo }) => {
-  return (
-    <h1>{courseInfo.name}</h1>
-  )
-}
+  return <h1>{courseInfo.name}</h1>;
+};
 
 const Content = ({ courseInfo }) => {
   return (
     <>
-      {courseInfo.parts.map((part, index) =>
-        <Part key={index} partName={part.name} exerciseAmount={part.exercises} />
-      )}
+      {courseInfo.parts.map((part, index) => (
+        <Part
+          key={index}
+          partName={part.name}
+          exerciseAmount={part.exercises}
+        />
+      ))}
     </>
-  )
-}
+  );
+};
 
 const Part = ({ partName, exerciseAmount }) => {
   return (
-    <p>{partName} {exerciseAmount}</p>
-  )
-}
+    <p>
+      {partName} {exerciseAmount}
+    </p>
+  );
+};
 
 const Total = ({ courseInfo }) => {
-  const total = courseInfo.parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
+  const total = courseInfo.parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
 
 const App = () => {
   const course = {
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10
+        name: "Fundamentals of React",
+        exercises: 10,
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7
+        name: "Using props to pass data",
+        exercises: 7,
       },
       {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header courseInfo={course} />
-      <Content courseInfo={course} /> 
+      <Content courseInfo={course} />
       <Total courseInfo={course} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
