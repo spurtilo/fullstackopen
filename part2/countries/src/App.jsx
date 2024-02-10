52ae8bf6c39a0de081e398ddf4bf2d284cd2ef17
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-02-10 19:36:20 +0200

    Complete exercise 2.19

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,50 +2,84 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const SearchField = ({ id, value, onChange }) => {
-  return <input id={id} value={value} onChange={onChange} />;
+  return <input id={id} value={value} onChange={onChange} autoFocus />;
 };
 
-const Country = ({ country }) => {
-  return <li key={country.cca3}>{country.name.common}</li>;
+const ShowButton = ({ buttonHandler, value }) => {
+  return (
+    <button type="button" onClick={buttonHandler} value={value}>
+      show
+    </button>
+  );
 };
 
-const Countries = ({ countries }) => {
-  if (
-    countries.length === 1 &&
-    countries[0].name.common !== "Too many matches, specify another filter."
-  ) {
+const Country = ({ country, buttonHandler }) => {
+  return (
+    <>
+      <li key={country.cca3}>
+        {country.name.common}{" "}
+        <ShowButton buttonHandler={buttonHandler} value={country.name.common} />
+      </li>
+    </>
+  );
+};
+
+const Countries = ({ countries, buttonHandler }) => {
+  if (countries.hasOwnProperty("tooManyMatches")) {
+    return countries.tooManyMatches;
+  } else if (countries.length === 1) {
     const chosenCountry = countries[0];
 
     return (
       <>
-        <h1>{chosenCountry.name.common}</h1>
-        <p>Capital: {chosenCountry.capital}</p>
-        <p>Area: {chosenCountry.area} square kilometers</p>
-        <p>Languages:</p>
-        {Object.values(chosenCountry.languages).map((value) => (
-          <p key={value}>{value}</p>
-        ))}
-        <img src={chosenCountry.flags.png} alt="Country flag" />
+        <h2>{chosenCountry.name.common}</h2>
+        <ul>
+          <li>
+            <b>Capital:</b> {chosenCountry.capital}
+          </li>
+          <li>
+            <b>Area:</b> {chosenCountry.area} square kilometers
+          </li>
+        </ul>
+        <h3>Languages:</h3>
+        <ul>
+          {Object.values(chosenCountry.languages).map((value) => (
+            <li key={value}>{value}</li>
+          ))}
+        </ul>
+        <img
+          src={chosenCountry.flags.png}
+          alt={`${chosenCountry.name.common} flag`}
+          className="flag"
+        />
       </>
     );
   }
 
   return countries.map((country) => (
-    <Country key={country.name.common} country={country} />
+    <Country
+      key={country.name.common}
+      country={country}
+      buttonHandler={buttonHandler}
+    />
   ));
 };
 
-function filterCountries(countriesArray, queryTerm) {
+function filterCountries(countriesArray, searchTerm) {
   let searchResult = [];
-  if (countriesArray && queryTerm.length > 0 && countriesArray.length) {
+  if (countriesArray && searchTerm.length > 0) {
     searchResult = countriesArray.filter((country) =>
-      country.name.common.toLowerCase().includes(queryTerm.toLowerCase().trim())
+      country.name.common
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase().trim())
     );
   }
+
   if (searchResult.length <= 10) {
     return searchResult;
+  } else {
+    return { tooManyMatches: "Too many matches, specify another filter." };
   }
-  return [{ name: { common: "Too many matches, specify another filter." } }];
 }
 
 const App = () => {
@@ -57,11 +91,19 @@ const App = () => {
       .get("https://studies.cs.helsinki.fi/restcountries/api/all")
       .then((response) => {
         setCountries(response.data);
+      })
+      .catch((error) => {
+        console.log("Error fetching countries:", error);
       });
   }, []);
 
-  const handleChange = (event) => {
+  const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleShowButtonClick = (event) => {
+    setSearchTerm(event.target.value);
+    console.log(event.target.value);
   };
 
   const countriesToShow = filterCountries(countries, searchTerm);
@@ -72,10 +114,13 @@ const App = () => {
       <SearchField
         id="countrySearch"
         value={searchTerm}
-        onChange={handleChange}
+        onChange={handleSearchChange}
       />
       <ul className="country-list">
-        <Countries countries={countriesToShow} />
+        <Countries
+          countries={countriesToShow}
+          buttonHandler={handleShowButtonClick}
+        />
       </ul>
     </div>
   );
