2b72f02d5f7b882f5600a9bf876aeb83a3ad5b13
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-02-11 13:52:32 +0200

    Complete exercise 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,7 +2,15 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const SearchField = ({ id, value, onChange }) => {
-  return <input id={id} value={value} onChange={onChange} autoFocus />;
+  return (
+    <input
+      id={id}
+      value={value}
+      onChange={onChange}
+      autoComplete="off"
+      autoFocus
+    />
+  );
 };
 
 const ShowButton = ({ buttonHandler, value }) => {
@@ -13,7 +21,82 @@ const ShowButton = ({ buttonHandler, value }) => {
   );
 };
 
-const Country = ({ country, buttonHandler }) => {
+const Weather = ({ country }) => {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const cityName = country.capital;
+  const countryCode = country.cca2;
+  const [weatherData, setWeatherData] = useState({
+    temp: null,
+    wind: null,
+    icon: null,
+  });
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryCode}&appid=${apiKey}&units=metric`
+      )
+      .then((response) => {
+        setWeatherData({
+          temp: response.data.main.temp,
+          wind: response.data.wind.speed,
+          icon: response.data.weather[0].icon,
+        });
+      })
+      .catch((error) => {
+        console.log("Error fetching weather data:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h3>Weather in {cityName}</h3>
+      <p>
+        <b>Temperature:</b>{" "}
+        {weatherData.temp !== null ? `${weatherData.temp} Celsius` : "N/A"}
+      </p>
+      {weatherData.icon && (
+        <img
+          src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
+        />
+      )}
+      <p>
+        <b>Wind:</b>{" "}
+        {weatherData.wind !== null ? `${weatherData.wind} m/s` : "N/A"}
+      </p>
+    </>
+  );
+};
+
+const CountryDetails = ({ country }) => {
+  return (
+    <>
+      <h2>{country.name.common}</h2>
+      <ul>
+        <li>
+          <b>Capital:</b> {country.capital}
+        </li>
+        <li>
+          <b>Area:</b> {country.area} square kilometers
+        </li>
+      </ul>
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(country.languages).map((value) => (
+          <li key={value}>{value}</li>
+        ))}
+      </ul>
+      <img
+        src={country.flags.png}
+        alt={`${country.name.common} flag`}
+        className="flag"
+      />
+      <Weather country={country} />
+    </>
+  );
+};
+
+const CountryListItem = ({ country, buttonHandler }) => {
   return (
     <>
       <li key={country.cca3}>
@@ -29,40 +112,23 @@ const Countries = ({ countries, buttonHandler }) => {
     return countries.tooManyMatches;
   } else if (countries.length === 1) {
     const chosenCountry = countries[0];
-
-    return (
-      <>
-        <h2>{chosenCountry.name.common}</h2>
-        <ul>
-          <li>
-            <b>Capital:</b> {chosenCountry.capital}
-          </li>
-          <li>
-            <b>Area:</b> {chosenCountry.area} square kilometers
-          </li>
-        </ul>
-        <h3>Languages:</h3>
-        <ul>
-          {Object.values(chosenCountry.languages).map((value) => (
-            <li key={value}>{value}</li>
-          ))}
-        </ul>
-        <img
-          src={chosenCountry.flags.png}
-          alt={`${chosenCountry.name.common} flag`}
-          className="flag"
-        />
-      </>
-    );
+    return <CountryDetails country={chosenCountry} />;
   }
 
   return countries.map((country) => (
-    <Country
-      key={country.name.common}
+    <CountryListItem
+      key={country.cca3}
       country={country}
       buttonHandler={buttonHandler}
     />
   ));
+};
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+  return <div className="error">{message}</div>;
 };
 
 function filterCountries(countriesArray, searchTerm) {
@@ -74,7 +140,6 @@ function filterCountries(countriesArray, searchTerm) {
         .includes(searchTerm.toLowerCase().trim())
     );
   }
-
   if (searchResult.length <= 10) {
     return searchResult;
   } else {
@@ -85,6 +150,7 @@ function filterCountries(countriesArray, searchTerm) {
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [countries, setCountries] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     axios
@@ -94,6 +160,12 @@ const App = () => {
       })
       .catch((error) => {
         console.log("Error fetching countries:", error);
+        setErrorMessage(
+          "Error fetching country data. Please reload the page or try again later."
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
       });
   }, []);
 
@@ -103,7 +175,6 @@ const App = () => {
 
   const handleShowButtonClick = (event) => {
     setSearchTerm(event.target.value);
-    console.log(event.target.value);
   };
 
   const countriesToShow = filterCountries(countries, searchTerm);
@@ -116,6 +187,7 @@ const App = () => {
         value={searchTerm}
         onChange={handleSearchChange}
       />
+      <Notification message={errorMessage} />
       <ul className="country-list">
         <Countries
           countries={countriesToShow}
