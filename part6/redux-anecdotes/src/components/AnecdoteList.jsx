b1c99b759dcd17c71d8fa11796346d47435b9da0
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-03-23 21:04:43 +0200

    Complete exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,14 @@ import { updateVotes } from "../reducers/anecdoteReducer";
 
 const AnecdoteList = () => {
   const dispatch = useDispatch();
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    if (filter === "") {
+      return anecdotes;
+    }
+    return anecdotes.filter((anecdote) =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase())
+    );
+  });
 
   const vote = (id) => dispatch(updateVotes(id));
 
