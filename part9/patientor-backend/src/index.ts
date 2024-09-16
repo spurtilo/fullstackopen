5cbacecbbbee4ce0aa46a3176811a7ad3a462474
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-09-16 14:53:07 +0300

    Complete exercises 9.8-9.9

---
+++ part9/patientor-backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { Request } from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(express.json());
+app.use(cors<Request>());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
